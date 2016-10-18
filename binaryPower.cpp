6e2d68ff7bcb3cc88c13900f3aa9559448b2512f
vladmosko <z38@mosko.ru>
2016-10-18 23:19:52 +0300

    Create binaryPower.cpp

---
+++ binaryPower.cpp
@@ -0,0 +1,26 @@
+template<typename T, typename Q> T binaryPower(T a, Q n) {
+    assert(n >= 0);
+    T res = 1;
+    while (n) {
+        if (n & 1) {
+            res = (res * a);
+        }
+        a = (a * a);
+        n >>= 1;
+    }
+    return res;
+}
+
+template<typename T, typename Q> T binaryPower(T a, Q n, T mod) {
+    assert(n >= 0 && mod > 0);
+    a %= mod;
+    T res = 1;
+    while (n) {
+        if (n & 1) {
+            res = (long long)res * a % mod;
+        }
+        a = (long long)a * a % mod;
+        n >>= 1;
+    }
+    return res;
+}
