7f7c6da28585184223efcf7c652ab4918de2864f
vladmosko <z38@mosko.ru>
2016-10-18 23:10:16 +0300

    Create eulerFunction.cpp

---
+++ eulerFunction.cpp
@@ -0,0 +1,15 @@
+template<typename T> T phi(T n) {
+	T res = n;
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0) {
+			while (n % i == 0) {
+				n /= i;
+			}
+			res -= res / i;
+		}
+	}
+	if (n > 1) {
+		res -= res / n;
+	}
+	return res;
+}
