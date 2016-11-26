a2df86af2f7088d46e10ee3b830830084448caa1
vladmosko <z38@mosko.ru>
2016-11-26 23:54:41 +0300

    Create funnySnippets.cpp

---
+++ funnySnippets.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+ double m[]= {7709179928849219.0, 771}; 
+int main() 
+{    
+      m[1]--?m[0]*=2,main():printf((char*)m);    
+}
+
+///
+
+main(a){printf(a,34,a="main(a){printf(a,34,a=%c%s%c,34);}",34);}
+
